ca628bc4fbd0c19906a6db5cff4350ce901c2c3f
goodidea-kp <33940472+goodidea-kp@users.noreply.github.com>
2026-01-01 09:10:19 -0800

    chore: implement toast notification, update Bulma and Leptos dependencies, and restructure documentation (#20)

--- lbc_catalog/src/elements/notification_example_page.rs
+++ lbc_catalog/src/elements/notification_example_page.rs
@@ -1,8 +1,12 @@
-use lbc::prelude::{Block, Content, HeaderSize, Notification, Title};
-use leptos::prelude::{IntoView, component, view};
+use lbc::prelude::{Block, Button, Content, HeaderSize, Notification, Title};
+use leptos::callback::Callback;
+use leptos::prelude::{AddAnyAttr, IntoView, Set, component, signal, view};
 
 #[component]
+#[allow(snake_case)]
 pub fn NotificationPage() -> impl IntoView {
+    let (open, set_open) = signal(false);
+
     view! {
         <Block>
             <Title size=HeaderSize::Is5>"Notification"</Title>
@@ -15,6 +19,18 @@ pub fn NotificationPage() -> impl IntoView {
                 </Notification>
                 <Notification classes="is-success is-light">
                     {"Success notification with light styling."}
+                </Notification>
+
+                <Title size=HeaderSize::Is5>"Toast Notification"</Title>
+                <Button on:click=move |_| set_open.set(true)>"Show Toast"</Button>
+                <Notification
+                    toast=true
+                    open=open
+                    set_open=Callback::new(move |val| set_open.set(val))
+                    auto_hide_ms=5000
+                    classes="is-primary"
+                >
+                    {"This is a toast notification that will autohide after 5 seconds."}
                 </Notification>
             </Content>
         </Block>
